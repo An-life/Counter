542e9b11775954f61440c655294bdb0070b09b56
Настя <ablovatskaja@gmail.com>
2021-09-30 15:04:41 +0300

    start task

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, MouseEvent} from 'react';
 import './App.css';
 
+
 function App() {
-  return (
+    let [value, setValue] = useState<number>(0);
+    let [disable, setDisable] = useState<{ [key: string]: boolean }>({bReset: true, bInc: false});
+
+    const incClick = (e: MouseEvent<HTMLButtonElement>) => {
+        if (e.currentTarget.id === '2') {
+            setDisable({...disable, bReset: true});
+            setValue(0)
+        } else if (value < 5) {
+            setDisable({...disable, bReset: false});
+            setValue(value + 1);
+        }
+}
+
+return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className={'container'}>
+
+        <div className={value===5?'valueContainer5':'valueContainer'}>{value}</div>
+            <div className={'buttonContainer'}>
+        <button  className={value===5?'disButton':'button'} id="1" onClick={(e) => incClick(e)} disabled={value===5?true:false}>inc</button>
+        <button className={disable.bReset?'disButton':'button'} id="2" onClick={(e) => incClick(e)} disabled={disable.bReset}>res</button>
+            </div>
+        </div>
     </div>
-  );
+);
 }
 
 export default App;
